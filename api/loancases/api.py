b27a8887a1eb0351e9c3bc9aff289ac206990a46
Mertin Kuo <mertin_k@hotmail.com>
2018-02-21 18:02:03 +0800

    add loancase

--- api/loancases/api.py
+++ api/loancases/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, g, request, current_app
+from flask import Blueprint, jsonify, g, request, current_app, make_response
 from loancaseAccess import loancaseAccess
 import json, os
 
@@ -9,8 +9,8 @@ def before_request():
     apiLoancases.dataAccess = loancaseAccess()
 
 @apiLoancases.route('/list', methods=['GET'])
-def get_all_requests():
-    current_app.logger.info('get_all_requests')
+def get_all_loancases():
+    current_app.logger.info('get_all_loancases')
     
     pageSize = request.args.get('pageSize', default =10, type = int)
     pageNumber = request.args.get('pageNumber', default = 1, type = int)
@@ -29,14 +29,115 @@ def get_all_requests():
         },
         "data" : []
     }
-
-    for l in loancases:
-        
+    
+    for loancase in loancases: 
         results["data"].append({
-            "_id":str(l["_id"]),
-            "idNumber":l["idNumber"],
-            "status":l["name"],
-            "status":l["status"],
+            "_id":str(loancase["_id"]),
+            "idNumber":loancase["idNumber"],
+            "name":loancase["name"],
+            "status":loancase["status"],
+            "applyDate":loancase["applyDate"],
+            "contactor":loancase["contactor"],
+            "sales":loancase["sales"],
+            "ticketCredit":loancase["ticketCredit"],
+            "salesVisitDate":loancase["salesVisitDate"],
+            "lastReplyDate":loancase["lastReplyDate"],
+            "createDate":loancase["createDate"],
+            "createUser":loancase["createUser"],
+            "modifyDate":loancase["modifyDate"],
+            "modifyUser":loancase["modifyUser"]
         })
 
+    return jsonify(results)
+
+@apiLoancases.route('/add', methods=['POST'])
+def add_loancase():
+    print('add_loancases')
+    data=json.loads(request.data)
+    print(data)
+    current_app.logger.info('add_loancases:'+ str(data))
+    
+    try:
+        apiLoancases.dataAccess.insert_loancase(data)
+    except:
+        current_app.logger.error('error add_loancases')
+        return make_response(jsonify("error"), 500) 
+
+    return jsonify("recevied")
+
+@apiLoancases.route('/update', methods=['PUT'])
+def update_loancase():
+    data=json.loads(request.data)
+    print(data)
+    current_app.logger.info('update_loancase:'+data['_id'])
+    ip =  request.remote_addr
+
+    try:
+        apiLoancases.dataAccess.update_loancase(data, ip)
+    except Exception as e:
+        current_app.logger.error(e)
+        return make_response(jsonify(str(e)), 500) 
+
+    return jsonify("updated")
+
+@apiLoancases.route('/key/<queryType>/<key>', methods=['GET'])
+def get_loancase_keyList(queryType,key):
+    print(queryType,key)
+    results = apiLoancases.dataAccess.get_loancases_keyList(queryType,key)
+
+    return jsonify(list(map(lambda x : x['_id'] ,list(results))))
+
+@apiLoancases.route('/check/<queryType>/<key>', methods=['GET'])
+def check_duplicate(queryType,key):
+    print(queryType,key)
+    results = apiLoancases.dataAccess.check_duplicate(queryType,key)
+
+    return jsonify(results)
+
+@apiLoancases.route('/<queryType>/<key>', methods=['GET'])
+def get_loancases(queryType,key):
+    current_app.logger.info('get_loancases')
+    
+    pageSize = request.args.get('pageSize', default =10, type = int)
+    pageNumber = request.args.get('pageNumber', default = 1, type = int)
+
+    try:
+        result = apiLoancases.dataAccess.get_loancases(queryType,key,pageSize,pageNumber)
+    except Exception as e:
+        current_app.logger.error(e)
+        return make_response(jsonify(str(e)), 500) 
+
+    loancases =  result['data']
+    totalCount = result["totalCount"]
+    totalPages = int( totalCount/ pageSize) + 1
+
+
+    results = {
+        "pagination" : {
+            "pageSize":pageSize,
+            "pageNumber":pageNumber,
+            "totalCount":totalCount,
+            "totalPages":totalPages
+        },
+        "data" : []
+    }
+    
+    for loancase in loancases: 
+        results["data"].append({
+            "_id":str(loancase["_id"]),
+            "idNumber":loancase["idNumber"],
+            "name":loancase["name"],
+            "status":loancase["status"],
+            "applyDate":loancase["applyDate"],
+            "contactor":loancase["contactor"],
+            "sales":loancase["sales"],
+            "ticketCredit":loancase["ticketCredit"],
+            "salesVisitDate":loancase["salesVisitDate"],
+            "lastReplyDate":loancase["lastReplyDate"],
+            "createDate":loancase["createDate"],
+            "createUser":loancase["createUser"],
+            "modifyDate":loancase["modifyDate"],
+            "modifyUser":loancase["modifyUser"]
+        })
+        
     return jsonify(results)
--- loancaseAccess.py
+++ loancaseAccess.py
@@ -1,9 +1,18 @@
 from DataAccess import DataAccess
+from bson.objectid import ObjectId
+from operator import  attrgetter
 import pymongo
+import datetime
 
 class loancaseAccess(DataAccess):
-    def add_loancase(self, loancase):
+    def insert_loancase(self, loancase):
         self.logger.logger.info('add loancase')
+        loancase['createDate'] = datetime.datetime.utcnow()
+        loancase['modifyDate'] = datetime.datetime.utcnow()
+        loancase['createUser'] = loancase['user']
+        loancase['modifyUser'] = loancase['user']
+        loancase.__delitem__('user')
+
         return self.db['Loancases'].insert(loancase)
 
     def get_allPaged_loancases(self, pageSize=10, pageNum=1):
@@ -17,6 +26,49 @@ class loancaseAccess(DataAccess):
        }
        return result
 
+    def update_loancase(self, loancase, ip):
+        print('update')
+
+        id = loancase['_id']
+        loancase['modifyDate'] = datetime.datetime.utcnow()
+        loancase['modifyUser'] = loancase['user']
+        loancase.__delitem__('user')
+        loancase.__delitem__('_id')
+    
+        self.db['LoancaseLog'].insert({
+            'action':'update',
+            'loancase':self.db['Loancases'].find_one({'_id': ObjectId(id)}),
+            'date' : datetime.datetime.utcnow(),
+            'user' :  loancase['modifyUser'],
+            'ip' : ip
+        })
+
+        return self.db['Loancases'].update({'_id':ObjectId(id)},loancase)
+
+    def get_loancases(self, queryType, key, pageSize=10, pageNum=1):
+        print('db get loancases')
+        skips = pageSize * (pageNum - 1)
+
+        print('123123')
+
+        data= self.db['Loancases'].aggregate([{'$match': {queryType:{'$regex':'^'+key}}}])
+        totalCount = len(list(data))
+
+        result = {
+            "totalCount":totalCount,
+            "data": list(self.db['Loancases'].aggregate([{'$match': {queryType:{'$regex':'^'+key}}},{ "$sort": {"createDate":-1}}, { "$skip": skips},{ "$limit": pageSize }]))
+        }
+
+        return result
+
+    def get_loancases_keyList(self, queryType, key):
+        print('/^'+key+'/')
+
+        return self.db['Loancases'].aggregate([{'$project':{queryType:1}},{'$group':{'_id':'$'+queryType}},{'$match':{'_id':{'$regex':'^'+key}}}])
+    
+    def check_duplicate(self, queryType, key):
+        return self.db['Loancases'].find({queryType:key}).count()
 
 if __name__ == "__main__":
     db = loancaseAccess()
+    print(db.get_allPaged_loancases()['data'][0])
